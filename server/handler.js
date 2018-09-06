365d138a0af72bf68bc20e1a428f5223c00607f5
Jonathon Gray <jonathon.d.gray@gmail.com>
2018-09-06 13:24:33 -0700

    serverless updates

--- server/handler.js
+++ server/handler.js
@@ -1,11 +1,76 @@
 const AWS = require("aws-sdk");
+const uuid = require("uuid");
+const dbTableName = "status";
+// const dynamoDb = require('./db/dynamodb');
+AWS.config.update({
+    region: process.env.R_AWS_REGION,
+    accessKeyId: process.env.R_AWS_KEY,
+    secretAccessKey: process.env.R_AWS_SECRET
+});
+const dynamoDb = new AWS.DynamoDB();
+
+const generatePolicy = (principalId, effect, resource) => {
+    let authResponse = {};
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        const policyDocument = {};
+        policyDocument.Version = '2012-10-17';
+        policyDocument.Statement = [];
+        const statementOne = {};
+        statementOne.Action = 'execute-api:Invoke';
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
+    }
+    return authResponse;
+}
+
+module.exports.auth = (event, context, callback) => {
+    // 9OJniHzRsRCApQbWy70gPfAbpHlPa8uc
+    callback(null, generatePolicy('demo-user', 'Allow', event.methodArn));
+};
 
 module.exports.getAllStatuses = (event, context, callback) => {
-
+    const params = {
+        TableName: dbTableName,
+        AttributesToGet: [
+            'id',
+            'buildingId',
+            'floorId',
+            'doorId',
+            'dt'
+        ]
+    };
+    
+    dynamoDb.scan(params, (error, result) => {
+        if (error) {
+            console.log(error);
+            callback('Failed getting status', error);
+        } else {
+            callback(null, JSON.stringify(result.Items));
+        }
+    });
 }
 
 module.exports.getStatusById = (event, context, callback) => {
-
+    const params = {
+        TableName: dbTableName,
+        Key: {
+            id: event.path.id,
+        },
+    };
+    
+    // fetch todo from the database
+    dynamoDb.get(params, (error, result) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback("Failed fetching status", null);
+            return;
+        }
+        callback(null, JSON.stringify(result.Item));
+    });
 }
 
 module.exports.createStatus = (event, context, callback) => {
@@ -27,24 +92,21 @@ module.exports.createStatus = (event, context, callback) => {
         callback('Door ID required', null);
     }
 
-    AWS.config.update({
-        region: "us-west-2",
-        accessKeyId: process.env.AWS_KEY,
-        secretAccessKey: process.env.AWS_SECRET
-    });
-    const ddb = new AWS.DynamoDB();
     const params = {
-        TableName: 'status',
+        TableName: dbTableName,
         Item: {
-            'buildingId': {N: data.buildingId},
-            'floorId' : {N: data.floorId},
-            'doorId' : {N: data.doorId},
-            'dt' : {S: ''}
+            'id': {S: uuid.v1()},
+            'buildingId': {N: data.buildingId.toString()},
+            'floorId' : {N: data.floorId.toString()},
+            'doorId' : {N: data.doorId.toString()},
+            'dt' : {S: new Date().getTime().toString()}
         }
     };
-    ddb.putItem(params, function(err, data) {
-        if (err) {
-            callback('Failed storing status', err);
+    const dynamoDb = new AWS.DynamoDB();
+    dynamoDb.putItem(params, function(error, data) {
+        if (error) {
+            console.log(error);
+            callback('Failed storing status', error);
         } else {
             callback(null, true);
         }
