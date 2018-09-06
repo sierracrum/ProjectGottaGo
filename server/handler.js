40b37b58b213f187f8d9473d37ce34f6c031464f
Jonathon Gray <jonathon.d.gray@gmail.com>
2018-09-06 10:15:54 -0700

    first server commit

---
+++ server/handler.js
@@ -0,0 +1,11 @@
+module.exports.getAllDoorStatuses = (event, context, callback) => {
+
+}
+
+module.exports.getStatusById = (event, context, callback) => {
+
+}
+
+module.exports.createStatus = (event, context, callback) => {
+
+}
