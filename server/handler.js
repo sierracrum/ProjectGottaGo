6d0240b1a2071937cc0fac69deea2feb4464a8f8
Jonathon Gray <jonathon.d.gray@gmail.com>
2018-09-06 10:35:35 -0700

    create status endpoint

--- server/handler.js
+++ server/handler.js
@@ -1,3 +1,5 @@
+const AWS = require("aws-sdk");
+
 module.exports.getAllStatuses = (event, context, callback) => {
 
 }
@@ -7,5 +9,44 @@ module.exports.getStatusById = (event, context, callback) => {
 }
 
 module.exports.createStatus = (event, context, callback) => {
+    const data = event.body;
 
+    if (!data) {
+        callback('Status data required', null);
+    }
+
+    if (!data.buildingId) {
+        callback('Building ID required', null);
+    }
+
+    if (!data.floorId) {
+        callback('Floor ID required', null);
+    }
+
+    if (!data.doorId) {
+        callback('Door ID required', null);
+    }
+
+    AWS.config.update({
+        region: "us-west-2",
+        accessKeyId: process.env.AWS_KEY,
+        secretAccessKey: process.env.AWS_SECRET
+    });
+    const ddb = new AWS.DynamoDB();
+    const params = {
+        TableName: 'status',
+        Item: {
+            'buildingId': {N: data.buildingId},
+            'floorId' : {N: data.floorId},
+            'doorId' : {N: data.doorId},
+            'dt' : {S: ''}
+        }
+    };
+    ddb.putItem(params, function(err, data) {
+        if (err) {
+            callback('Failed storing status', err);
+        } else {
+            callback(null, true);
+        }
+    }); 
 }
