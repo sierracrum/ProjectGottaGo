f3554d6c514257441c5b519e05a7c3616ca9dde7
Heather Knowlton <heatherknowlton79@gmail.com>
2018-09-06 15:01:07 -0700

    removing headers and adding status to body

--- pi/index.js
+++ pi/index.js
@@ -1,25 +1,22 @@
-var gpio = require('rpi-gpio');
-var axios = require('axios');
-require('dotenv').load();
+var gpio = require("rpi-gpio");
+var axios = require("axios");
+require("dotenv").load();
 
 var laststate = 1;
 
 gpio.setup(process.env.GPIO_PORT, gpio.DIR_IN, readInput);
 
 function readInput() {
-  gpio.read(process.env.GPIO_PORT, function (err, value) {
+  gpio.read(process.env.GPIO_PORT, function(err, value) {
     if (laststate != value) {
-      axios.post(process.env.API_URL, {
+      axios
+        .post(process.env.API_URL, {
           buildingId: 1,
           floorId: 1,
           doorId: 1,
-        }, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.AUTH_TOKEN}`
-          }
+          status: laststate
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
         });
     }
