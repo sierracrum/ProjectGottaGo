fb343198788c97b41b152f9b2c30b034adba843d
Tae Min Pak <taeminpak@gmail.com>
2018-09-07 09:32:38 -0700

    fixed index

--- pi/index.js
+++ pi/index.js
@@ -2,25 +2,48 @@ var gpio = require("rpi-gpio");
 var axios = require("axios");
 require("dotenv").load();
 
-var laststate = 1;
+var currentstate;
+var laststate;
 
 gpio.setup(process.env.GPIO_PORT, gpio.DIR_IN, readInput);
 
 function readInput() {
   gpio.read(process.env.GPIO_PORT, function(err, value) {
-    if (laststate != value) {
-      axios
-        .post(process.env.API_URL, {
-          buildingId: 1,
-          floorId: 1,
-          doorId: 1,
-          status: laststate
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    }
-    value === true ? (laststate = 1) : (laststate = 0);
+    //find the current state
+    value === true ? (currentstate = 1) : (currentstate = 0);
   });
+  //if the current state changes, update the last state, and post to the database
+  if (laststate != currentstate) {
+    axios
+      .post(process.env.API_URL, {
+        buildingId: 1,
+        floorId: 1,
+        doorId: 1,
+        status: currentstate
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    laststate = currentstate;
+  }
   setTimeout(readInput, 1000); //recheck door every second
 }
+
+// function readInput() {
+//   gpio.read(process.env.GPIO_PORT, function(err, value) {
+//     if (currentstate != value) {
+//       axios
+//         .post(process.env.API_URL, {
+//           buildingId: 1,
+//           floorId: 1,
+//           doorId: 1,
+//           status: currentstate
+//         })
+//         .catch(function(error) {
+//           console.log(error);
+//         });
+//     }
+//     value === true ? (currentstate = 1) : (currentstate = 0);
+//   });
+//   setTimeout(readInput, 1000); //recheck door every second
+// }
